98eb38e8b1cb2be626e7d10bfebb8d31c66bdf6a
Dasun Sanjaya <dasunsanjaya080@gmail.com>
2026-01-07 17:32:18 +0530

    ros workspace build and create a launch file

---
+++ course_1/build/my_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dasunsanjaya080/ROS2_works/course_1/src/my_robot_description
+# Build directory: /home/dasunsanjaya080/ROS2_works/course_1/build/my_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
