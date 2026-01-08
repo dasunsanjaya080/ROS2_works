9214c6321f2c1e0cbdd4c4feb89a389c0f82ad6a
Dasun Sanjaya <dasunsanjaya080@gmail.com>
2026-01-08 09:59:24 +0530

    simulate with Gazebo

---
+++ course_1/build/my_robot_bringup/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dasunsanjaya080/ROS2_works/course_1/src/my_robot_bringup
+# Build directory: /home/dasunsanjaya080/ROS2_works/course_1/build/my_robot_bringup
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ course_1/build/my_robot_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/dasunsanjaya080/ROS2_works/course_1/install/my_robot_description/share/my_robot_description/package.sh"
