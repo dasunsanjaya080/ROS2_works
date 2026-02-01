f734d635ef4a70f88c5b62e77dce109a12a492e7
Dasun Sanjaya <dasunsanjaya080@gmail.com>
2026-02-01 20:02:04 +0530

    Update ROS launch files and configs

---
+++ course_2/build/turtlebot3_fake_node/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/dasunsanjaya080/ROS2_works/course_2/src/turtlebot3_fake_node
+# Build directory: /home/dasunsanjaya080/ROS2_works/course_2/build/turtlebot3_fake_node
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ course_2/build/turtlebot3_simulations/colcon_command_prefix_build.sh
@@ -0,0 +1,4 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/dasunsanjaya080/ROS2_works/course_2/install/turtlebot3_fake_node/share/turtlebot3_fake_node/package.sh"
+. "/home/dasunsanjaya080/ROS2_works/course_2/install/turtlebot3_gazebo/share/turtlebot3_gazebo/package.sh"
+. "/home/dasunsanjaya080/ROS2_works/course_2/install/turtlebot3_manipulation_gazebo/share/turtlebot3_manipulation_gazebo/package.sh"
